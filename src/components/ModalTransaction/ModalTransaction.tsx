598a4ba7b102157e9bfd718e075cceb268558ab9
GBelther <belthergabriel@gmail.com>
2021-06-12 11:23:54 -0300

    fix: retirada de lixo

--- src/components/ModalTransaction/ModalTransaction.tsx
+++ src/components/ModalTransaction/ModalTransaction.tsx
@@ -5,8 +5,6 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
-import { FiX } from "react-icons/fi";
-
 import {
   Container,
   Header,
